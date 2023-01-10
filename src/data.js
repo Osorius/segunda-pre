ac774a0b3474bb5b63cd3e224277028db71dd609
Eduardo Palacios <eduardopalacios94@hotmail.es>
2023-01-10 16:34:16 -0300

    se agrega proyect

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://www.lifeder.com/wp-content/uploads/2019/09/Figura_1_Hericium_erinaceus_64176.jpg',
+		nameProduct: 'Melena de Leon',
+		price: 1800,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://cdn.shortpixel.ai/spai/q_lossy+ret_img+to_webp/https://premiumspores.com/wp-content/uploads/2021/03/cordyceps-militaris-liquid-culture.jpg',
+		nameProduct: 'Cordyceps Militaris',
+		price: 1500,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://www.researchgate.net/publication/337611870/figure/fig1/AS:919123655684098@1596147564717/Fruto-del-hongo-Pleurotus-Ostreatus-o-girgola-Fuente-http-wwwmykowebcom.png',
+		nameProduct: 'Girgola Gris',
+		price: 1090,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_723830-MLA51742903354_092022-O.webp',
+		nameProduct: 'Girgola Amarilla',
+		price: 1550,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://s1.eestatic.com/2020/10/13/ciencia/nutricion/nutricion_527957676_162425936_1024x576.jpg',
+		nameProduct: 'Reishi',
+		price: 1320,
+		quantity: 5,
+	},
+	{
+		id: 6,
+		img: 'https://www.investigacionyciencia.es/images/52240/imageFacebookThumbnail.jpg',
+		nameProduct: 'Trufa',
+		price: 1520,
+		quantity: 1,
+	},
+];
